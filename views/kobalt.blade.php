9b0058d7608ae5ae161ecfba382c37ead35f13c8
Vince <71010397+Somgur1@users.noreply.github.com>
2022-01-23 19:55:30 +0100

    Add files via upload

---
+++ views/kobalt.blade.php
@@ -0,0 +1,50 @@
+
+
+<p>hello world</p>
+@csrf
+
+@php
+
+
+    $collection = collect(["arne", "brenda"]);
+
+$b = 0;
+       $i = 0;
+
+
+  // foreach ( $collection as $namen)
+   //{
+        foreach ($collection as $namen)
+            {
+       $naam = chunk_split($collection, 1, ' ');
+
+
+                $i++;
+
+
+
+
+
+                }
+echo $i;
+
+echo $naam;
+
+
+
+     //  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @endphp
+
